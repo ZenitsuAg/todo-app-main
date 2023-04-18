0c08c303db801c94ee958f4d707b60b3464db65c
Fred Robins <99406968+ZenitsuAg@users.noreply.github.com>
2023-04-18 08:24:17 +0000

    Add files via upload
    Some update to the entire project because some components weren't working as expected.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'desktop-dark':   "url('./images/bg-desktop-dark.jpg')",
-        'desktop-light':  "url('./images/bg-desktop-light.jpg')",
-        'mobile-dark':    "url('./images/bg-mobile-dark.jpg')",
-        'mobile-light':   "url('./images/bg-mobile-light.jpg')",
-        'icon-check':     "url('./images/icon-check.svg')",
-        'icon-cross':     "url('./images/icon-cross.svg')",
-        'icon-moon':      "url('./images/icon-moon.svg')",
-        'icon-sun':       "url('./images/icon-sun.svg')",
+        'desktop-dark':   "url('/images/bg-desktop-dark.jpg')",
+        'desktop-light':  "url('/images/bg-desktop-light.jpg')",
+        'mobile-dark':    "url('/images/bg-mobile-dark.jpg')",
+        'mobile-light':   "url('/images/bg-mobile-light.jpg')",
+        'icon-check':     "url('/images/icon-check.svg')",
+        'icon-cross':     "url('/images/icon-cross.svg')",
+        'icon-moon':      "url('/images/icon-moon.svg')",
+        'icon-sun':       "url('/images/icon-sun.svg')",
       },
       fontFamily: {
         JosefinSans: ["Josefin Sans",]
